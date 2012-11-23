bc109ad509ff1dbafa9826cf2432a556ebb65b21
shepherdwind <eward.song@gmail.com>
2012-11-23 10:58:43 +0800

    update exmaple, fix bug @ie6

--- test/web/v.js
+++ test/web/v.js
@@ -12,8 +12,6 @@ KISSY.config({
 });
 
 KISSY.use('velocity/index, web/directives', function(S, Velocity, asts){
-  //var html = (S.all('#tpl').html());
-  //var asts = Parser.parse(html);
   var compile = new Velocity(asts);
-  console.log(compile.render());
+  S.all('body').html(compile.render());
 });
