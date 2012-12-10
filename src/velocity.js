51cb321536d4af2a000b100e0513e703bc12ebda
shepherdwind <eward.song@gmail.com>
2012-12-10 11:03:08 +0800

    add export for Helper

--- src/velocity.js
+++ src/velocity.js
@@ -1,17 +1,23 @@
-var Parser = require('./parse/');
-var utils = require('./utils');
+var Parser  = require('./parse/');
+var utils   = require('./utils');
 var Compile = require('./compile/');
+var Helper  = {};
+
+Helper.Structure = require('./helper/structure');
+Helper.Jsonify   = require('./helper/Jsonify');
+Helper.BackStep  = require('./helper/backstep');
+
 var Velocity = {
-  Parser: Parser,
-  Compile: Compile
+  Parser  : Parser,
+  Compile : Compile,
+  Helper  : Helper
 };
 
 Velocity.render = function(template, context){
-  var t1 = Date.now();
+  var t1   = Date.now();
   var asts = Parser.parse(template);
-  var t2 = Date.now();
-  var str = 'parse syntax tree finished, cost time: ' + (t2 - t1)+ 'ms.';
-  console.log(str);
+  var t2   = Date.now();
+  var str  = 'parse syntax tree finished, cost time: ' + (t2 - t1)+ 'ms.';
 
   var compile = new Compile(asts);
   return compile.render(context);
