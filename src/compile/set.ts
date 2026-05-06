b9a030d02a579b31ada6670a82acc03d308d35fc
eward <eward.song@gmail.com>
2026-05-06 14:54:07 +0800

    fix: guard set paths against prototype pollution

--- src/compile/set.ts
+++ src/compile/set.ts
@@ -1,6 +1,26 @@
-import type { SetAST, VELOCITY_AST } from '../type';
+import type { Attribute, ReferencesAST, SetAST, VELOCITY_AST } from '../type';
 import { applyMixins } from '../utils';
 import { Compile } from './base-compile';
+
+const PROTO_KEY = '__proto__';
+const PROTOTYPE_CHAIN_KEYS = new Set(['constructor', 'prototype']);
+
+type SetPathTarget = {
+  blocked: boolean;
+  rootRef?: unknown;
+  shouldSetRoot?: boolean;
+  baseRef?: unknown;
+  key?: string;
+};
+
+function hasOwnProperty(baseRef: unknown, key: string): boolean {
+  return (
+    (typeof baseRef === 'object' || typeof baseRef === 'function') &&
+    baseRef !== null &&
+    Object.prototype.hasOwnProperty.call(baseRef, key)
+  );
+}
+
 /**
  * #set value
  */
@@ -20,6 +40,71 @@ export class SetValue extends Compile {
     // not find local variable, return global context
     return this.context;
   }
+  private getPathKey(exp: Attribute): string {
+    if (exp.type === 'property') {
+      return exp.id;
+    }
+
+    if (exp.type === 'index' && exp.id) {
+      return String(this.getLiteral(exp.id as VELOCITY_AST));
+    }
+
+    return '';
+  }
+
+  private isBlockedPathKey(baseRef: unknown, key: string, isEnd: boolean): boolean {
+    if (key === PROTO_KEY) {
+      return true;
+    }
+
+    if (key === 'prototype' && typeof baseRef === 'function') {
+      return true;
+    }
+
+    return !isEnd && PROTOTYPE_CHAIN_KEYS.has(key) && !hasOwnProperty(baseRef, key);
+  }
+
+  private resolveSetPath(context: object, ref: ReferencesAST): SetPathTarget {
+    if (!ref.path) {
+      return { blocked: ref.id === PROTO_KEY };
+    }
+
+    if (ref.id === PROTO_KEY) {
+      return { blocked: true };
+    }
+
+    let rootRef = (context as Record<string, unknown>)[ref.id];
+    let shouldSetRoot = false;
+
+    if (typeof rootRef !== 'object') {
+      rootRef = {};
+      shouldSetRoot = true;
+    }
+
+    let baseRef = rootRef;
+
+    for (let i = 0; i < ref.path.length; i++) {
+      if (baseRef === undefined || baseRef === null) {
+        return { blocked: false, rootRef, shouldSetRoot };
+      }
+
+      const key = this.getPathKey(ref.path[i]);
+      const isEnd = ref.path.length === i + 1;
+
+      if (this.isBlockedPathKey(baseRef, key, isEnd)) {
+        return { blocked: true };
+      }
+
+      if (isEnd) {
+        return { blocked: false, rootRef, shouldSetRoot, baseRef, key };
+      }
+
+      baseRef = (baseRef as Record<string, unknown>)[key];
+    }
+
+    return { blocked: false, rootRef, shouldSetRoot };
+  }
+
   /**
    * parse #set
    */
@@ -31,6 +116,12 @@ export class SetValue extends Compile {
     if (this.contextId && this.contextId.indexOf('macro:') === 0) {
       context = this.context;
       // fix #129
+    }
+
+    const setPath = this.resolveSetPath(context, ref);
+
+    if (setPath.blocked) {
+      return;
     }
 
     const valAst = ast.equal[1];
@@ -48,42 +139,13 @@ export class SetValue extends Compile {
       return;
     }
 
-    let baseRef = (context as Record<string, unknown>)[ref.id];
-    if (typeof baseRef !== 'object') {
-      baseRef = {};
+    if (setPath.shouldSetRoot) {
+      (context as Record<string, unknown>)[ref.id] = setPath.rootRef;
     }
 
-    (context as Record<string, unknown>)[ref.id] = baseRef;
-    const len = ref.path ? ref.path.length : 0;
-
-    ref.path.some((exp, i) => {
-      const isEnd = len === i + 1;
-      let key: string;
-
-      if (exp.type === 'property') {
-        key = exp.id;
-      } else if (exp.type === 'index' && exp.id) {
-        key = String(this.getLiteral(exp.id as VELOCITY_AST));
-      } else {
-        key = '';
-      }
-
-      if (isEnd) {
-        (baseRef as Record<string, unknown>)[key] = val;
-        return true;
-      }
-
-      baseRef = (baseRef as Record<string, unknown>)[key] as Record<string, unknown>;
-
-      // such as
-      // #set($a.d.c2 = 2)
-      // but $a.d is undefined, value set fail
-      if (baseRef === undefined) {
-        return true;
-      }
-
-      return false;
-    });
+    if (setPath.key !== undefined) {
+      (setPath.baseRef as Record<string, unknown>)[setPath.key] = val;
+    }
   }
 }
 
